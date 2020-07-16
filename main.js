ae48ca09cf6fd2e972465524ed48917523099af1
besher58 <beshr58@gmail.com>
2020-07-16 15:49:03 +0200

    array-split

---
+++ main.js
@@ -0,0 +1,9 @@
+let meinText1='Hallo Welt. Ich wünsche euch allen einen guten Arbeitstag.'
+let meinText2='Wie geht es dir heute?'
+let meinText3='Heute is ªª ein großer Tag für uns.'
+let a=meinText1.split()
+let b=meinText2.split('')
+let c=meinText3.split(' ')
+console.log(a)
+console.log(b)
+console.log(c)
